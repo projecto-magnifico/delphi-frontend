e5848496023cb42bfbafbffba92c0ac65c4371b3
Andrea Zerio <andreazerio@Andreas-MacBook-Pro.local>
2017-10-24 14:17:36 +0100

    adding Breaking News side component and setting up Earth component

---
+++ delphi-frontend/src/components/NewsCard.js
@@ -0,0 +1,34 @@
+import React from 'react'
+import './css/NewsCard.css'
+
+class NewsCard extends React.Component {
+    constructor(props) {
+        super(props)
+        
+    }
+
+    
+    
+    render() {
+       
+        return (
+            <div id="particles">
+                <div id ='newsCard'className= "uk-card-default uk-card-hover ">
+                    <div id="card">
+                        <div id="newsHeader" className="uk-card-header">
+                            <span> <h3 className="uk-card-title"> <img id="sourceIcon" src="http://icons.veryicon.com/ico/System/Circle/bbc%20news.ico" /> Heading</h3></span>
+                        </div>
+                        <div className="uk-card uk-card-body" id="newsBody">
+                            <p id="newsDescription" className="uk-card-body">"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."</p>
+                        </div>
+                        <div className="uk-card-footer">
+                        <button className="uk-button uk-button-primary uk-button-small">Follow</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default NewsCard
